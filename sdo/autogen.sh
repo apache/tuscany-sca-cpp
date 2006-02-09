31d69fc62178adf9a85e3816f806ab63b78a46ff
Peter George Robbins <robbinspg@apache.org>
2006-02-09 10:24:40 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@376248 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sdo/autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+for i in libtoolize aclocal autoconf autoheader
+do
+    echo -n "Running $i..."
+    $i || exit 1
+    echo 'done.'
+done
+  
+echo -n 'Running automake...'
+automake --add-missing
+echo 'done.'
+exit 0
+
