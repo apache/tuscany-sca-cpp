5bc50d68b90aa29dd39d90b02674c4bb197ef157
Peter George Robbins <robbinspg@apache.org>
2006-03-10 11:33:50 +0000

    TUSCANY-108 Add wrappers/proxies for test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@384769 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sca/runtime/core/test/CustomerInfo/CustomerInfoImpl_CustomerInfoService_Proxy.cpp
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "CustomerInfoImpl_CustomerInfoService_Proxy.h"
+
+#include "osoa/sca/sca.h"
+using namespace osoa::sca;
+using namespace tuscany::sca;
+
+extern "C"
+{
+
+    #if defined(WIN32) || defined(_WINDOWS)
+    __declspec(dllexport) 
+    #endif
+    CustomerInfoImpl_CustomerInfoService_Proxy* CustomerInfoImpl_CustomerInfoService_Proxy_Factory(ServiceWrapper* target)
+    {
+        return new CustomerInfoImpl_CustomerInfoService_Proxy(target);
+    }
+
+    #if defined(WIN32) || defined(_WINDOWS)
+    __declspec(dllexport) 
+    #endif
+    void CustomerInfoImpl_CustomerInfoService_Proxy_Destructor(void* proxy)
+    {
+        delete (CustomerInfoImpl_CustomerInfoService_Proxy*)proxy;
+    }
+}
+
+CustomerInfoImpl_CustomerInfoService_Proxy::CustomerInfoImpl_CustomerInfoService_Proxy(ServiceWrapper* targ) : target(targ)
+{
+}
+
+CustomerInfoImpl_CustomerInfoService_Proxy::~CustomerInfoImpl_CustomerInfoService_Proxy()
+{
+    if (target)
+        delete target;
+}
+
+const char* CustomerInfoImpl_CustomerInfoService_Proxy::getCustomerInformation(const char* arg0)
+{
+    Operation operation("getCustomerInformation", 1);
+    operation.setParameter(0, (void*)arg0);
+
+ 
+    target->invoke(operation);
+    return (const char*)operation.getReturnValue();
+
+}
+
+
