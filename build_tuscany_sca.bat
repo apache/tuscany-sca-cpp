9faefa4faa27ee8bd569b3bf9b507eba2b042ff7
Edward Slattery <edslattery@apache.org>
2006-06-27 11:25:50 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@417430 13f79535-47bb-0310-9956-ffa450edef68

--- build_tuscany_sca.bat
+++ build_tuscany_sca.bat
@@ -18,23 +18,23 @@ if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1 mkdir %TO_SRC%\tuscany_sca-0
 
 if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects 
 
-xcopy/s c:\apache\checkout_rc1\sca\projects %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects
+xcopy/s %FROM_DIR%\sca\projects %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects
 
 if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime 
 
-xcopy/s c:\apache\checkout_rc1\sca\runtime %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime
+xcopy/s %FROM_DIR%\sca\runtime %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime
 
 if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools 
 
-xcopy/s c:\apache\checkout_rc1\sca\tools %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools
+xcopy/s %FROM_DIR%\sca\tools %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools
 
 if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples 
 
-xcopy/s c:\apache\checkout_rc1\sca\samples %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples
+xcopy/s %FROM_DIR%\sca\samples %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples
 
 if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd 
 
-xcopy/s c:\apache\checkout_rc1\sca\xsd %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd
+xcopy/s %FROM_DIR%\sca\xsd %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd
 
 
 rem install libxml2 to c:\apache\libs
@@ -52,9 +52,9 @@ set TUSCANY_SDOCPP=%TO_BIN%\tuscany_sdo-bin-0.1.incubating-M1\deploy
 
 echo Building SCA....
 
-set TUSCANY_SCACPP=c:\apache\checkout_rc1\sca\deploy
+set TUSCANY_SCACPP=%FROM_DIR%\sca\deploy
 
-cd c:\apache\checkout_rc1\sca\projects\tuscany_sca
+cd %FROM_DIR%\sca\projects\tuscany_sca
 
 call build
 
@@ -62,7 +62,7 @@ echo Building scagen...
 
 set JAVA_HOME=c:\jvm5\ibm5
 
-cd c:\apache\checkout_rc1\sca\tools\scagen
+cd %FROM_DIR%\sca\tools\scagen
 
 call ant
 
@@ -75,16 +75,16 @@ if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1 mkdir %TO_BIN%\tuscany_s
 if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy 
 if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools 
 
-xcopy/s c:\apache\checkout_rc1\sca\deploy %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy
-copy c:\apache\checkout_rc1\sca\INSTALL   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
-copy c:\apache\checkout_rc1\sca\LICENSE   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
-copy c:\apache\checkout_rc1\sca\COPYING   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
+xcopy/s %FROM_DIR%\sca\deploy %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy
+copy %FROM_DIR%\sca\INSTALL   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
+copy %FROM_DIR%\sca\LICENSE   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
+copy %FROM_DIR%\sca\COPYING   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
 
-xcopy/s c:\apache\checkout_rc1\sca\tools\scagen\bld %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools
+xcopy/s %FROM_DIR%\sca\tools\scagen\bld %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools
 
 echo Building calculator....
 
-cd c:\apache\checkout_rc1\sca\samples\ides\devstudio6\projects\Calculator
+cd %FROM_DIR%\sca\samples\ides\devstudio6\projects\Calculator
 
 call build
 
@@ -94,8 +94,8 @@ if not exist %TO_BIN%\sca\samples mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-
 if not exist %TO_BIN%\sca\samples\ides mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\ides
 if not exist %TO_BIN%\sca\samples\Calculator mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator
 
-xcopy/s c:\apache\checkout_rc1\sca\samples\ides %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\ides
-xcopy/s c:\apache\checkout_rc1\sca\samples\Calculator %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator
+xcopy/s %FROM_DIR%\sca\samples\ides %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\ides
+xcopy/s %FROM_DIR%\sca\samples\Calculator %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator
  
 set TUSCANY_SCACPP_SYSTEM_ROOT=%TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator\deploy
 set TUSCANY_SCACPP_DEFAULT_MODULE=CalculatorSubsystem/CalculatorService
