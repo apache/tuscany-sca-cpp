b932cad0f8bdc102ee8f523486d7af9cd3c93a25
Peter George Robbins <robbinspg@apache.org>
2006-06-29 09:46:56 +0000

    TUSCANY-458 SCA windows distro build script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@417976 13f79535-47bb-0310-9956-ffa450edef68

--- build_tuscany_sca.bat
+++ build_tuscany_sca.bat
@@ -1,58 +1,75 @@
 
 @echo off
+setlocal
+if "%LIBXML2_HOME%" == "" (
+echo "LIBXML2_HOME not set"
+goto end
+)
+echo using LIBXML2: %LIBXML2_HOME%
 
-echo Build started....
+if "%ICONV_HOME%" == "" (
+echo "ICONV_HOME not set"
+goto end
+)
+echo using ICONV: %ICONV_HOME%"
+
+if "%AXIS2C_HOME%" == "" (
+echo "AXIS2C_HOME not set"
+goto end
+)
+
+if "%TUSCANY_SDOCPP%" == "" (
+echo "TUSCANY_SDOCPP not set"
+goto end
+)
+ 
+echo using TUSCANY_SDOCPP: %TUSCANY_SDOCPP%
 
 call vcvars32
 
-set FROM_DIR=c:\apache\checkout_rc1
+echo Build of SCA started....
+set PACKAGE=tuscany_sca_cpp
+set VERSION=0.1.incubating-M1
+set SRC_PACKAGE=%PACKAGE%-%VERSION%-src
+set BIN_PACKAGE=%PACKAGE%-%VERSION%-bin
 
-set TO_SRC=c:\installsrc
+set FROM_DIR=%cd%
+set TO_SRC=%FROM_DIR%
 
-set TO_BIN=c:\installbin
+set TO_BIN=%FROM_DIR%
 
 echo Copying SCA source...
 
 if not exist %TO_SRC% mkdir %TO_SRC%
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1 mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1 
 
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects 
+rmdir /S /Q %TO_SRC%\%SRC_PACKAGE%
+if not exist %TO_SRC%\%SRC_PACKAGE% mkdir %TO_SRC%\%SRC_PACKAGE% 
 
-xcopy/s %FROM_DIR%\sca\projects %TO_SRC%\tuscany_sca-0.1.incubating-M1\projects
+if not exist %TO_SRC%\%SRC_PACKAGE%\projects mkdir %TO_SRC%\%SRC_PACKAGE%\projects 
 
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime 
+xcopy/s %FROM_DIR%\sca\projects %TO_SRC%\%SRC_PACKAGE%\projects
 
-xcopy/s %FROM_DIR%\sca\runtime %TO_SRC%\tuscany_sca-0.1.incubating-M1\runtime
+if not exist %TO_SRC%\%SRC_PACKAGE%\runtime mkdir %TO_SRC%\%SRC_PACKAGE%\runtime 
 
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools 
+xcopy/s %FROM_DIR%\sca\runtime %TO_SRC%\%SRC_PACKAGE%\runtime
 
-xcopy/s %FROM_DIR%\sca\tools %TO_SRC%\tuscany_sca-0.1.incubating-M1\tools
+if not exist %TO_SRC%\%SRC_PACKAGE%\tools mkdir %TO_SRC%\%SRC_PACKAGE%\tools 
 
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples 
+xcopy/s %FROM_DIR%\sca\tools %TO_SRC%\%SRC_PACKAGE%\tools
 
-xcopy/s %FROM_DIR%\sca\samples %TO_SRC%\tuscany_sca-0.1.incubating-M1\samples
+if not exist %TO_SRC%\%SRC_PACKAGE%\samples mkdir %TO_SRC%\%SRC_PACKAGE%\samples 
+if not exist %TO_SRC%\%SRC_PACKAGE%\samples\Calculator mkdir %TO_SRC%\%SRC_PACKAGE%\samples\Calculator
+if not exist %TO_SRC%\%SRC_PACKAGE%\samples\ides mkdir %TO_SRC%\%SRC_PACKAGE%\samples\ides
 
-if not exist %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd mkdir %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd 
+xcopy/s %FROM_DIR%\sca\samples\Calculator %TO_SRC%\%SRC_PACKAGE%\samples\Calculator
+xcopy/s %FROM_DIR%\sca\samples\ides %TO_SRC%\%SRC_PACKAGE%\samples\ides
 
-xcopy/s %FROM_DIR%\sca\xsd %TO_SRC%\tuscany_sca-0.1.incubating-M1\xsd
+if not exist %TO_SRC%\%SRC_PACKAGE%\xsd mkdir %TO_SRC%\%SRC_PACKAGE%\xsd 
 
+xcopy/s %FROM_DIR%\sca\xsd %TO_SRC%\%SRC_PACKAGE%\xsd
 
-rem install libxml2 to c:\apache\libs
-set LIBXML2_HOME=c:\apache\libs\libxml2-2.6.19.win32
-
-rem install iconv to c:\apache\libs
-set ICONV_HOME=c:\apache\libs\iconv-1.9.1.win32
-
-rem install axis2c
-set AXIS2C_HOME=c:\axis\axis2c_92\axis2c-bin-0.92-win32
-
-echo Assuming SDO already built.
-
-set TUSCANY_SDOCPP=%TO_BIN%\tuscany_sdo-bin-0.1.incubating-M1\deploy
 
 echo Building SCA....
-
-set TUSCANY_SCACPP=%FROM_DIR%\sca\deploy
 
 cd %FROM_DIR%\sca\projects\tuscany_sca
 
@@ -60,27 +77,25 @@ call build
 
 echo Building scagen...
 
-set JAVA_HOME=c:\jvm5\ibm5
-
 cd %FROM_DIR%\sca\tools\scagen
 
 call ant
 
+copy %FROM_DIR%\sca\tools\scagen\bld\scagen.jar  %FROM_DIR%\sca\deploy\bin
+copy %FROM_DIR%\sca\tools\scagen\bld\scagen.cmd  %FROM_DIR%\sca\deploy\bin
+
 echo Installing SCA....
 
-set TUSCANY_SCACPP=%TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy
-
 if not exist %TO_BIN% mkdir %TO_BIN%
-if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1 mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1 
-if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy 
-if not exist %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools 
+rmdir /S /Q %TO_BIN%\%BIN_PACKAGE%
+if not exist %TO_BIN%\%BIN_PACKAGE% mkdir %TO_BIN%\%BIN_PACKAGE%
 
-xcopy/s %FROM_DIR%\sca\deploy %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\deploy
-copy %FROM_DIR%\sca\INSTALL   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
-copy %FROM_DIR%\sca\LICENSE   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
-copy %FROM_DIR%\sca\COPYING   %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1
-
-xcopy/s %FROM_DIR%\sca\tools\scagen\bld %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\tools
+xcopy/s %FROM_DIR%\sca\deploy %TO_BIN%\%BIN_PACKAGE%
+copy %FROM_DIR%\sca\INSTALL   %TO_BIN%\%BIN_PACKAGE%
+copy %FROM_DIR%\sca\LICENSE   %TO_BIN%\%BIN_PACKAGE%
+copy %FROM_DIR%\sca\COPYING   %TO_BIN%\%BIN_PACKAGE%
+copy %FROM_DIR%\sca\NOTICE    %TO_BIN%\%BIN_PACKAGE%
+copy %FROM_DIR%\sca\README    %TO_BIN%\%BIN_PACKAGE%
 
 echo Building calculator....
 
@@ -88,28 +103,13 @@ cd %FROM_DIR%\sca\samples\ides\devstudio6\projects\Calculator
 
 call build
 
-if not exist %TO_BIN% mkdir %TO_BIN%
-if not exist %TO_BIN%\sca mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1 
-if not exist %TO_BIN%\sca\samples mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples
-if not exist %TO_BIN%\sca\samples\ides mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\ides
-if not exist %TO_BIN%\sca\samples\Calculator mkdir %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator
+if not exist %TO_BIN%\%BIN_PACKAGE%\samples mkdir %TO_BIN%\%BIN_PACKAGE%\samples
+if not exist %TO_BIN%\%BIN_PACKAGE%\samples\ides mkdir %TO_BIN%\%BIN_PACKAGE%\samples\ides
+if not exist %TO_BIN%\%BIN_PACKAGE%\samples\Calculator mkdir %TO_BIN%\%BIN_PACKAGE%\samples\Calculator
 
-xcopy/s %FROM_DIR%\sca\samples\ides %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\ides
-xcopy/s %FROM_DIR%\sca\samples\Calculator %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator
- 
-set TUSCANY_SCACPP_SYSTEM_ROOT=%TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator\deploy
-set TUSCANY_SCACPP_DEFAULT_MODULE=CalculatorSubsystem/CalculatorService
+xcopy/s %FROM_DIR%\sca\samples\ides %TO_BIN%\%BIN_PACKAGE%\samples\ides
+xcopy/s %FROM_DIR%\sca\samples\Calculator %TO_BIN%\%BIN_PACKAGE%\samples\Calculator
 
-cd %TO_BIN%\tuscany_sca-bin-0.1.incubating-M1\samples\Calculator\deploy\modules\Calculator
-
-set PATH=%TUSCANY_SDOCPP%\bin;%TUSCANY_SCACPP%\bin;%PATH%
-
-echo %TUSCANY_SDOCPP%
-echo %TUSCANY_SCACPP%
-echo %TUSCANY_SCACPP_SYSTEM_ROOT%
-
-echo Testing SCA...
-
-call Calc add 34 56
-
+:end
 echo SCA Build complete.
+endlocal
