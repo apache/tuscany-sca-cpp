e70b759d88a6689d593d698ce406fb00c42a43a9
Peter George Robbins <robbinspg@apache.org>
2006-06-29 11:27:40 +0000

    TUSCANY-458 Restructure Calculator sample for distribution build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@418014 13f79535-47bb-0310-9956-ffa450edef68

--- samples/Calculator/WSClient/runwsclient.cmd
+++ samples/Calculator/WSClient/runwsclient.cmd
@@ -1,1 +1,13 @@
-WSClient\Debug\WSClient div 22 7
+@echo off
+setlocal
+
+if "%AXIS2C_HOME%" == "" (
+echo "AXIS2C_HOME not set"
+goto end
+)
+echo using Axis2C: %AXIS2C_HOME%"
+
+rem Run the client
+WSClient.exe %*
+
+endlocal
