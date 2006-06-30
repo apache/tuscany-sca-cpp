012016ca0294abfa1195099af81fba7559108a56
Peter George Robbins <robbinspg@apache.org>
2006-06-30 12:51:54 +0000

    TUSCANY-458 remove windows build warnings
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@418252 13f79535-47bb-0310-9956-ffa450edef68

--- sca/runtime/core/test/MyValue/MyValueImpl.cpp
+++ sca/runtime/core/test/MyValue/MyValueImpl.cpp
@@ -17,6 +17,10 @@
 
 /* $Rev$ $Date: 2005/12/22 11:33:21 $ */
 
+#if defined(WIN32) || defined (_WINDOWS)
+#pragma warning(disable: 4786)
+#endif 
+
 #include "MyValueImpl.h"
 #include "CustomerInfo.h"
 #include "StockQuoteService.h"
