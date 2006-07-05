08949ba8699c2e7db941023680bb9c08b4730453
Peter George Robbins <robbinspg@apache.org>
2006-07-05 16:56:21 +0000

    TUSCANY-517 Update to calculator sample provided by Andy Borley
    add files I missed first time :-(
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@419278 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sca/samples/Calculator/CalculatorModule/DivideService.h
@@ -0,0 +1,30 @@
+/* 
++----------------------------------------------------------------------+
+| (c) Copyright IBM Corporation 2005.                                  | 
+| All Rights Reserved.                                                 |
++----------------------------------------------------------------------+ 
+|                                                                      | 
+| Licensed under the Apache License, Version 2.0 (the "License"); you  | 
+| may not use this file except in compliance with the License. You may | 
+| obtain a copy of the License at                                      | 
+|  http://www.apache.org/licenses/LICENSE-2.0                          |
+|                                                                      | 
+| Unless required by applicable law or agreed to in writing, software  | 
+| distributed under the License is distributed on an "AS IS" BASIS,    | 
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      | 
+| implied. See the License for the specific language governing         | 
+| permissions and limitations under the License.                       | 
++----------------------------------------------------------------------+ 
+*/
+
+#ifndef DIVIDESERVICE_H
+#define DIVIDESERVICE_H
+class DivideService  
+{
+public:
+	virtual float divide(float arg1, float arg2) = 0;
+};
+
+#endif
+
+
---
+++ sca/samples/Calculator/CalculatorModule/DivideServiceImpl_DivideService_Wrapper.h
@@ -0,0 +1,40 @@
+/* 
+ *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef DivideServiceImpl_DivideService_Wrapper_h
+#define DivideServiceImpl_DivideService_Wrapper_h
+
+#if defined(WIN32) || defined (_WINDOWS)
+#pragma warning(disable: 4786)
+#endif 
+
+#include "DivideServiceImpl.h"
+#include "tuscany/sca/core/ComponentServiceWrapper.h"
+
+class DivideServiceImpl_DivideService_Wrapper : public tuscany::sca::ComponentServiceWrapper
+{
+public:
+    DivideServiceImpl_DivideService_Wrapper(tuscany::sca::model::Service* target);
+    virtual ~DivideServiceImpl_DivideService_Wrapper();
+    virtual void invokeService(tuscany::sca::Operation& operation);
+    virtual void* newImplementation();
+    virtual void deleteImplementation();
+private:
+    DivideServiceImpl* impl;
+};
+
+#endif // DivideServiceImpl_DivideService_Wrapper_h
+
