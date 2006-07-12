4a23b2bdf342ea84a88578a44fb33b3c8b541fc5
Peter George Robbins <robbinspg@apache.org>
2006-07-12 14:05:14 +0000

    TUSCANY-534 add licence info to .bat/cmd files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@421275 13f79535-47bb-0310-9956-ffa450edef68

--- build_tuscany_sdo.bat
+++ build_tuscany_sdo.bat
@@ -1,5 +1,20 @@
-
 @echo off
+
+
+@REM  Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+@REM
+@REM  Licensed under the Apache License, Version 2.0 (the "License");
+@REM  you may not use this file except in compliance with the License.
+@REM  You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM  Unless required by applicable law or agreed to in writing, software
+@REM  distributed under the License is distributed on an "AS IS" BASIS,
+@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM  See the License for the specific language governing permissions and
+@REM  limitations under the License.
+
 setlocal
 if "%LIBXML2_HOME%" == "" (
 echo "LIBXML2_HOME not set"
@@ -55,11 +70,11 @@ copy %FROM_DIR%\COPYING   %TO_SRC%\%SRC_PACKAGE%
 copy %FROM_DIR%\NOTICE    %TO_SRC%\%SRC_PACKAGE%
 copy %FROM_DIR%\README    %TO_SRC%\%SRC_PACKAGE%
 copy %FROM_DIR%\GettingStarted.html  %TO_SRC%\%SRC_PACKAGE%
+copy %FROM_DIR%\build.bat  %TO_SRC%\%SRC_PACKAGE%
 
 echo Building SDO....
 
-cd %FROM_DIR%\projects\tuscany_sdo
-
+cd %FROM_DIR%
 call build
 
 echo Installing SDO....
