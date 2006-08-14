3dc0efeb060673e4dc9f4987322c717f1c12891c
Peter George Robbins <robbinspg@apache.org>
2006-08-14 10:51:46 +0000

    TUSCANY-613 Applying Andy's patch (TUSCANY-613-2.patch)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@431322 13f79535-47bb-0310-9956-ffa450edef68

--- sca/samples/BigBank/deploy.bat
+++ sca/samples/BigBank/deploy.bat
@@ -1,16 +1,36 @@
-copy Accounts\Debug\Accounts.dll Deploy\composites\Accounts
-copy Accounts\Debug\Accounts.pdb Deploy\composites\Accounts
-copy Accounts\AccountServiceImpl.h Deploy\composites\Accounts
-copy Accounts\AccountService.h Deploy\composites\Accounts
-copy Accounts\AccountDataServiceImpl.h Deploy\composites\Accounts
-copy Accounts\AccountDataService.h Deploy\composites\Accounts
-copy Accounts\StockQuoteService.h Deploy\composites\Accounts
-copy Accounts\StockQuoteServiceImpl.h Deploy\composites\Accounts
-copy Accounts\StockQuoteExternalService.h Deploy\composites\Accounts
-copy AccountClient\Debug\AccountClient.exe Deploy\composites\Accounts
-copy AccountClient\Debug\AccountClient.pdb Deploy\composites\Accounts
-copy Accounts\sca.composite Deploy\composites\Accounts
-copy Accounts\sca.subsystem Deploy\subsystems\Accounts
-copy Accounts\AccountServiceImpl.componentType Deploy\composites\Accounts
-copy Accounts\AccountDataServiceImpl.componentType Deploy\composites\Accounts
-copy Accounts\StockQuoteServiceImpl.componentType Deploy\composites\Accounts
+if not exist deploy mkdir deploy
+if not exist deploy\composites mkdir deploy\composites
+if not exist deploy\composites\Accounts mkdir deploy\composites\Accounts
+if not exist deploy\subsystems mkdir deploy\subsystems
+if not exist deploy\subsystems\Accounts mkdir deploy\subsystems\Accounts
+if not exist deploy\bin mkdir deploy\bin
+
+copy Accounts\Debug\Accounts.dll deploy\composites\Accounts
+copy Accounts\Debug\Accounts.pdb deploy\composites\Accounts
+copy Accounts\AccountServiceImpl.h deploy\composites\Accounts
+copy Accounts\AccountService.h deploy\composites\Accounts
+copy Accounts\AccountDataServiceImpl.h deploy\composites\Accounts
+copy Accounts\AccountDataService.h deploy\composites\Accounts
+copy Accounts\StockQuoteService.h deploy\composites\Accounts
+copy Accounts\StockQuoteServiceImpl.h deploy\composites\Accounts
+copy Accounts\StockQuoteWebService.h deploy\composites\Accounts
+copy Accounts\Accounts.composite deploy\composites\Accounts
+copy Accounts\AccountServiceImpl.componentType deploy\composites\Accounts
+copy Accounts\AccountDataServiceImpl.componentType deploy\composites\Accounts
+copy Accounts\StockQuoteServiceImpl.componentType deploy\composites\Accounts
+copy Accounts\AccountService.wsdl deploy\composites\Accounts
+copy Accounts\StockQuoteService.wsdl deploy\composites\Accounts
+copy Accounts\StockQuoteTypes.xsd deploy\composites\Accounts
+copy Accounts\Tuscany-model.config deploy\composites\Accounts
+
+copy BigBankSubsystem\BigBankSubsystem.composite deploy\subsystems\Accounts
+
+copy AccountClient\Debug\AccountClient.exe deploy\bin
+copy AccountClient\Debug\AccountClient.pdb deploy\bin
+copy AccountClient\runclient.cmd deploy\bin
+
+copy WSAccountClient\Debug\WSAccountClient.exe deploy\bin
+copy WSAccountClient\Debug\WSAccountClient.pdb deploy\bin
+copy WSAccountClient\runwsclient.cmd deploy\bin
+
+
