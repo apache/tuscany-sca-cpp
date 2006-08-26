b2d0040d6aaa248c36e8c97706a959872a4bd391
Peter George Robbins <robbinspg@apache.org>
2006-08-26 07:23:43 +0000

    First pass at extension loading
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@437107 13f79535-47bb-0310-9956-ffa450edef68

---
+++ runtime/core/src/tuscany/sca/extension/ImplementationExtension.h
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef tuscany_sca_extension_implementationextension_h
+#define tuscany_sca_extension_implementationextension_h
+
+#include "osoa/sca/export.h"
+#include <string>
+using std::string;
+
+#include <tuscany/sca/model/Component.h>
+#include <tuscany/sca/model/Service.h>
+#include <tuscany/sca/core/ServiceWrapper.h>
+
+#include <commonj/sdo/SDO.h>
+
+namespace tuscany
+{
+    namespace sca
+    {
+        class SCA_API ImplementationExtension 
+        {
+        public:
+            /**
+            * Default constructor
+            */
+            ImplementationExtension();            
+            
+            /**
+            * Destructor
+            */
+            virtual ~ImplementationExtension();            
+                      
+            /**
+            * return the name of the extension (e.g. "cpp" "php")
+            */
+            virtual const string& getExtensionName() = 0;
+                      
+            /**
+            * return the QName of schema elemant for this implementation extension
+            * (e.g. "http://www.osoa.org/xmlns/sca/1.0#implementation.cpp")
+            */
+            virtual const string& getImplementationTypeQName() = 0;
+
+            virtual void loadModelElement(commonj::sdo::DataObjectPtr scdlImplementation, model::Component* component) = 0;
+
+            virtual ServiceWrapper* getServiceWrapper(model::Service* service) = 0;
+         };
+
+        
+    } // End namespace sca
+} // End namespace tuscany
+
+
+typedef tuscany::sca::ImplementationExtension* (* TUSCANY_IMPLEMENTATION_EXTENSION_FACTORY) ();
+
+#endif // tuscany_sca_extension_implementationextension_h
+
