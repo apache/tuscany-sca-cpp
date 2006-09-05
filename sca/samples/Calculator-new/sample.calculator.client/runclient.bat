a40fc72f72b6dc6c1671c9491e23a26a6b7b73fb
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-09-05 16:23:50 +0000

    Cleaned up version of Calculator, following the naming conventions used in the SCA spec documents, simplifying interface and implementation names, simplifying the names used in WSDL, and a few other minor cleanup type changes to the .bat and README
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@440385 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sca/samples/Calculator-new/sample.calculator.client/runclient.bat
@@ -0,0 +1,47 @@
+@echo off
+
+@REM  Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+@REM
+@REM  Licensed under the Apache License, Version 2.0 (the "License");
+@REM  you may not use this file except in compliance with the License.
+@REM  You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM  Unless required by applicable law or agreed to in writing, software
+@REM  distributed under the License is distributed on an "AS IS" BASIS,
+@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM  See the License for the specific language governing permissions and
+@REM  limitations under the License.
+
+setlocal
+
+if "%TUSCANY_SCACPP%" == "" (
+echo "TUSCANY_SCACPP not set"
+goto end
+)
+echo using SCA installed at %TUSCANY_SCACPP%
+
+if "%TUSCANY_SDOCPP%" == "" (
+echo "TUSCANY_SDOCPP not set"
+goto end
+)
+echo using SDO installed at %TUSCANY_SDOCPP%
+
+if "%AXIS2C_HOME%" == "" (
+echo "AXIS2C_HOME not set"
+goto end
+)
+echo using Axis2C installed at %AXIS2C_HOME%
+
+set TUSCANY_SCACPP_SYSTEM_ROOT=%~d0%~p0\..\
+set TUSCANY_SCACPP_DEFAULT_COMPONENT=sample.CalculatorComponent
+
+set PATH=%TUSCANY_SCACPP%\bin;%TUSCANY_SCACPP%\extensions\cpp\bin;%TUSCANY_SDOCPP%\bin;%AXIS2C_HOME%\lib;%PATH%
+
+.\calculator_client.exe add 4.7 9
+.\calculator_client.exe div 7.2 3.6
+.\calculator_client.exe mul 7 6
+
+:end
+endlocal
---
+++ sca/samples/Calculator-new/sample.calculator/DivideImpl.cpp
@@ -0,0 +1,44 @@
+/*
+ *
+ *  Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+#include "DivideImpl.h"
+#include <stdio.h>
+
+DivideImpl::DivideImpl()
+{
+}
+    
+DivideImpl::~DivideImpl()
+{
+}
+
+// Divide interface
+float DivideImpl::divide(float arg1, float arg2)
+{
+    if(arg2 == 0.0)
+    {
+        printf("DivideImpl::div %f / %f !! Cannot divide by zero, so returning 0\n", arg1, arg2);
+        return 0;
+    }
+
+    float result = arg1 / arg2;
+    printf("DivideImpl::div %f / %f = %f\n", arg1, arg2, result);
+    return result;
+}
+	
+
