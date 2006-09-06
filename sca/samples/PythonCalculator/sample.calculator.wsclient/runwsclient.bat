77852e696e1205ff9791727507ee461f47734b9b
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-09-06 09:54:52 +0000

    Added a new Python Calculator sample and some print statements to the Ruby Calculator sample
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@440672 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sca/samples/PythonCalculator/sample.calculator.wsclient/runwsclient.bat
@@ -0,0 +1,31 @@
+@echo off
+
+@REM  Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+@REM
+@REM  Licensed under the Apache License, Version 2.0 (the "License");
+@REM  you may not use this file except in compliance with the License.
+@REM  You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM  Unless required by applicable law or agreed to in writing, software
+@REM  distributed under the License is distributed on an "AS IS" BASIS,
+@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM  See the License for the specific language governing permissions and
+@REM  limitations under the License.
+
+setlocal
+
+if "%AXIS2C_HOME%" == "" (
+echo "AXIS2C_HOME not set"
+goto end
+)
+echo Using Axis2C installed at %AXIS2C_HOME%"
+
+rem Run the client
+.\calculator_wsclient.exe add 4.7 9
+.\calculator_wsclient.exe div 7.2 3.6
+.\calculator_wsclient.exe mul 7 6
+
+:end
+endlocal
