apache/tuscany-sca-cpp

commit 81fcc598564ffb80fec515afe1dc4b26c47af897

Added support for int and unsigned int native types to Operation & classes that use Operation.
git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@448122 13f79535-47bb-0310-9956-ffa450edef68

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/Axis2Client.cpp
@@ -290,6 +290,11 @@ namespace tuscany
                                         l.append(*(short*)parm.getValue());
                                         break;
                                     }
+                                case Operation::INT: 
+                                    {
+                                        l.append(*(int*)parm.getValue());
+                                        break;
+                                    }
                                 case Operation::LONG: 
                                     {
                                         l.append(*(long*)parm.getValue());
@@ -297,12 +302,17 @@ namespace tuscany
                                     }
                                 case Operation::USHORT: 
                                     {
-                                        l.append(*(short*)parm.getValue());
+                                        l.append(*(unsigned short*)parm.getValue());
+                                        break;
+                                    }
+                                case Operation::UINT: 
+                                    {
+                                        l.append(*(unsigned int*)parm.getValue());
                                         break;
                                     }
                                 case Operation::ULONG: 
                                     {
-                                        l.append(*(long*)parm.getValue());
+                                        l.append(*(unsigned long*)parm.getValue());
                                         break;
                                     }
                                 case Operation::FLOAT: 
@@ -358,6 +368,11 @@ namespace tuscany
                                     requestDO->setShort(i, *(short*)parm.getValue());
                                     break;
                                 }
+                            case Operation::INT: 
+                                {
+                                    requestDO->setInteger(i, *(int*)parm.getValue());
+                                    break;
+                                }
                             case Operation::LONG: 
                                 {
                                     requestDO->setLong(i, *(long*)parm.getValue());
@@ -366,6 +381,11 @@ namespace tuscany
                             case Operation::USHORT: 
                                 {
                                     requestDO->setInteger(i, *(unsigned short*)parm.getValue());
+                                    break;
+                                }
+                            case Operation::UINT: 
+                                {
+                                    requestDO->setInteger(i, *(unsigned int*)parm.getValue());
                                     break;
                                 }
                             case Operation::ULONG: 

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/WSServiceProxy.cpp
@@ -209,7 +209,7 @@ namespace tuscany
                             case Type::DataObjectType:
                                 {
                                     DataObjectPtr dataObjectData = inputDataObject->getDataObject(pl[i]);
-                                    //printf("inputDataObject has DataObjectType named %s (#%d)\n", name, dataObjectData);
+                                    printf("inputDataObject has DataObjectType named %s (#%d)\n", name, dataObjectData);
                     
                                     if(!dataObjectData)
                                     {
@@ -225,7 +225,7 @@ namespace tuscany
                                      * Get each element as a DataObject and add in to the parameter list
                                      */
                     
-                                    //printf("inputDataObject has OpenDataObjectType named %s\n", name);
+                                    printf("inputDataObject has OpenDataObjectType named %s\n", name);
                                     DataObjectList& dataObjectList = inputDataObject->getList(pl[i]);
                                     
                                     for(int j=0; j<dataObjectList.size(); j++)
@@ -379,6 +379,11 @@ namespace tuscany
                                 l.append(*(short*)operation.getReturnValue());
                                 break;
                             }
+                        case Operation::INT: 
+                            {
+                                l.append(*(int*)operation.getReturnValue());
+                                break;
+                            }
                         case Operation::LONG: 
                             {
                                 l.append(*(long*)operation.getReturnValue());
@@ -387,6 +392,11 @@ namespace tuscany
                         case Operation::USHORT: 
                             {
                                 l.append(*(short*)operation.getReturnValue());
+                                break;
+                            }
+                        case Operation::UINT: 
+                            {
+                                l.append(*(int*)operation.getReturnValue());
                                 break;
                             }
                         case Operation::ULONG: 
@@ -457,14 +467,24 @@ namespace tuscany
                                 outputDataObject->setShort(pl[i], *(short*)operation.getReturnValue());
                                 break;
                             }
+                        case Operation::INT: 
+                            {
+                                outputDataObject->setInteger(pl[i], *(int*)operation.getReturnValue());
+                                break;
+                            }
                         case Operation::LONG: 
                             {
-                                outputDataObject->setLong(pl[i], *(long*)operation.getReturnValue());
+                                outputDataObject->setInteger(pl[i], *(long*)operation.getReturnValue());
                                 break;
                             }
                         case Operation::USHORT: 
                             {
                                 outputDataObject->setInteger(pl[i], *(unsigned short*)operation.getReturnValue());
+                                break;
+                            }
+                        case Operation::UINT: 
+                            {
+                                outputDataObject->setInteger(pl[i], *(unsigned int*)operation.getReturnValue());
                                 break;
                             }
                         case Operation::ULONG: 

sca/runtime/core/src/tuscany/sca/core/Operation.cpp
@@ -114,6 +114,12 @@ namespace tuscany
             parameters.insert(parameters.end(), Parameter((void*)parm, SHORT));
         }
 
+        void Operation::addParameter(const int *parm)
+        {
+            LOGINFO(4, "Operation::addParameter(int)");
+            parameters.insert(parameters.end(), Parameter((void*)parm, INT));
+        }
+
         void Operation::addParameter(const long *parm)
         {
             LOGINFO(4, "Operation::addParameter(long)");
@@ -124,6 +130,12 @@ namespace tuscany
         {
             LOGINFO(4, "Operation::addParameter(unsigned short)");
             parameters.insert(parameters.end(), Parameter((void*)parm, USHORT));
+        }
+
+        void Operation::addParameter(const unsigned int *parm)
+        {
+            LOGINFO(4, "Operation::addParameter(unsigned int)");
+            parameters.insert(parameters.end(), Parameter((void*)parm, UINT));
         }
 
         void Operation::addParameter(const unsigned long *parm)
@@ -200,6 +212,12 @@ namespace tuscany
             returnValue = Parameter((void*)parm, SHORT);
         }
 
+        void Operation::setReturnValue(const int *parm)
+        {
+            LOGINFO(4, "Operation::setReturnValue(int)");
+            returnValue = Parameter((void*)parm, INT);
+        }
+
         void Operation::setReturnValue(const long *parm)
         {
             LOGINFO(4, "Operation::setReturnValue(long*)");
@@ -210,6 +228,12 @@ namespace tuscany
         {
             LOGINFO(4, "Operation::setReturnValue(unsigned short*)");
             returnValue = Parameter((void*)parm, USHORT);
+        }
+
+        void Operation::setReturnValue(const unsigned int *parm)
+        {
+            LOGINFO(4, "Operation::setReturnValue(unsigned int)");
+            returnValue = Parameter((void*)parm, UINT);
         }
 
         void Operation::setReturnValue(const unsigned long *parm)

sca/runtime/core/src/tuscany/sca/core/Operation.h
@@ -65,8 +65,10 @@ namespace tuscany
                 VOID_TYPE,
                 BOOL,
                 SHORT,
+                INT,
                 LONG,
                 USHORT,
+                UINT,
                 ULONG,
                 FLOAT,
                 DOUBLE,
@@ -96,8 +98,10 @@ namespace tuscany
             SCA_API void setReturnValue(const void *retVal);
             SCA_API void setReturnValue(const bool *retVal);
             SCA_API void setReturnValue(const short *retVal);
+            SCA_API void setReturnValue(const int *retVal);
             SCA_API void setReturnValue(const long *retVal);
             SCA_API void setReturnValue(const unsigned short *retVal);
+            SCA_API void setReturnValue(const unsigned int *retVal);
             SCA_API void setReturnValue(const unsigned long *retVal);
             SCA_API void setReturnValue(const float *retVal);
             SCA_API void setReturnValue(const double *retVal);
@@ -115,8 +119,10 @@ namespace tuscany
             SCA_API void addParameter(const void *parm);
             SCA_API void addParameter(const bool *parm);
             SCA_API void addParameter(const short *parm);
+            SCA_API void addParameter(const int *parm);
             SCA_API void addParameter(const long *parm);
             SCA_API void addParameter(const unsigned short *parm);
+            SCA_API void addParameter(const unsigned int *parm);
             SCA_API void addParameter(const unsigned long *parm);
             SCA_API void addParameter(const float *parm);
             SCA_API void addParameter(const double *parm);

sca/runtime/extensions/python/src/tuscany/sca/python/PythonServiceWrapper.cpp
@@ -328,6 +328,16 @@ namespace tuscany
                                     pValue = PyInt_FromLong(*(unsigned short*)parm.getValue());
     				                break;
     			                }
+    			                case Operation::INT: 
+    			                {
+                                    pValue = PyInt_FromLong(*(int*)parm.getValue());
+    				                break;
+    			                }
+    			                case Operation::UINT: 
+    			                {
+                                    pValue = PyInt_FromLong(*(unsigned int*)parm.getValue());
+    				                break;
+    			                }
     			                case Operation::LONG: 
     			                {
                                     pValue = PyLong_FromLong(*(long*)parm.getValue());

commit a5a58816ca10b4c4abd3365e5cc8042973b673f9

Fixed build error introduced with r448122 about DataObjectList.append not supporting unsigned short type on Linux
git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@448144 13f79535-47bb-0310-9956-ffa450edef68

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/Axis2Client.cpp
@@ -302,7 +302,7 @@ namespace tuscany
                                     }
                                 case Operation::USHORT: 
                                     {
-                                        l.append(*(unsigned short*)parm.getValue());
+                                        l.append(*(short*)parm.getValue());
                                         break;
                                     }
                                 case Operation::UINT: 

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/WSServiceProxy.cpp
@@ -391,7 +391,7 @@ namespace tuscany
                             }
                         case Operation::USHORT: 
                             {
-                                l.append(*(unsigned short*)operation.getReturnValue());
+                                l.append(*(short*)operation.getReturnValue());
                                 break;
                             }
                         case Operation::UINT: 