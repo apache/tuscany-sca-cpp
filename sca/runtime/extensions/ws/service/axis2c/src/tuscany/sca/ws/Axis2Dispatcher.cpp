apache/tuscany-sca-cpp

commit 515f1aa8b433dca291df26715605ad03e35d5ba2

Ported Web Service binding, samples and scenarios to Web Service Binding spec level 0.96
git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@448463 13f79535-47bb-0310-9956-ffa450edef68

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/Axis2Service.cpp
@@ -296,7 +296,7 @@ Axis2Service_invoke(axis2_svc_skeleton_t *svc_skeleton,
                         try {
                             wsdlOperation =  wsdlDefinition->findOperation(
                                 binding->getServiceName(),
-                                binding->getPortName(),
+                                binding->getEndpointName(),
                                 op_name);
                         }
                         catch(SystemConfigurationException &ex)

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/Axis2Client.cpp
@@ -104,7 +104,7 @@ namespace tuscany
                     {
                         wsdlOperation =  wsdlDefinition->findOperation(
                             binding->getServiceName(),
-                            binding->getPortName(),
+                            binding->getEndpointName(),
                             operationName);
                     }
                     catch(SystemConfigurationException &ex)
@@ -191,9 +191,16 @@ namespace tuscany
                 axis2_options_t* options = axis2_options_create(env);
                 AXIS2_OPTIONS_SET_TO(options, env, endpoint_ref);
                 int soap_version = AXIOM_SOAP11;
-                if (wsdlOperation.getSoapVersion() == WSDLOperation::SOAP12)
+                if (binding->getSOAPVersion() == "1.2")
                 {
                     soap_version = AXIOM_SOAP12;
+                }
+                else
+                {
+                    if (wsdlOperation.getSoapVersion() == WSDLOperation::SOAP12)
+                    {
+                        soap_version = AXIOM_SOAP12;
+                    }
                 }
                 
                 AXIS2_OPTIONS_SET_SOAP_VERSION(options, env, soap_version);
@@ -265,7 +272,7 @@ namespace tuscany
                     DataObjectPtr requestDO = dataFactory->create(wsdlOp.getInputTypeUri().c_str(), 
                         wsdlOp.getInputTypeName().c_str());
 
-                    // Go through data object to set the return value
+                    // Go through data object to set the input parameters
                     PropertyList pl = requestDO->getType().getProperties();
                 
                     if(pl.size() == 0)

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/WSServiceBindingExtension.cpp
@@ -72,9 +72,19 @@ namespace tuscany
                 if (bindingType == "WebServiceBinding")
                 {
                     string uri = scdlBinding->getCString("uri");
-                    string port = scdlBinding->getCString("port");
+                    string endpoint = scdlBinding->getCString("endpoint");
+                    commonj::sdo::DataObjectList& soap = scdlBinding->getList("soapbinding");
+                    string version;
+                    if (soap.size()!=0)
+                    {
+                        version = soap.getCString(0);
+                    }
+                    else
+                    {
+                        version = "";
+                    }
 
-                    WSServiceBinding* serviceBinding = new WSServiceBinding(service, uri, port);
+                    WSServiceBinding* serviceBinding = new WSServiceBinding(service, uri, endpoint, version);
                     
                     return serviceBinding;
                 }

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/model/WSServiceBinding.cpp
@@ -30,59 +30,59 @@ namespace tuscany
         {
 
             // Constructor
-            WSServiceBinding::WSServiceBinding(Service* service, const string& uri, const string& port)
-                : ServiceBinding(service, uri), port(port)
+            WSServiceBinding::WSServiceBinding(Service* service, const string& uri, const string& endpoint, const string& version)
+                : ServiceBinding(service, uri), endpoint(endpoint), soapVersion(version)
             {
-                parsePort();
+                parseEndpoint();
                 
                 serviceWrapper = new WSServiceWrapper(service);
             }
 
-            void WSServiceBinding::parsePort()
+            void WSServiceBinding::parseEndpoint()
             {
-                // Port is of the form: <wsdl-namepace-uri>#wsdl.endpoint(<service-name>/<port-name>)
-                string::size_type hash = port.find("#");
+                // Endpoint is of the form: <wsdl-namepace-uri>#wsdl.endpoint(<service-name>/<endpoint-name>)
+                string::size_type hash = endpoint.find("#");
                 if (hash != string::npos)
                 {
                     // Found a hash
 
                     // Namepace is the part before the #
-                    wsdlNamespaceURL = port.substr(0, hash);
+                    wsdlNamespaceURL = endpoint.substr(0, hash);
 
                     
-                    if ( (hash+1) < port.length())
+                    if ( (hash+1) < endpoint.length())
                     {
                         // Check the next part is wsdl.endpoint( 
                         int ending = hash+15;
-                        string check = port.substr(hash+1, 14);
+                        string check = endpoint.substr(hash+1, 14);
                         if (check.compare("wsdl.endpoint(") == 0)
                         {
                             // Find the matching )
-                            int endBracket = port.find(")",ending);
+                            int endBracket = endpoint.find(")",ending);
                             if (endBracket-1 > ending+1)
                             {
-                                string serviceAndPort = port.substr(ending, endBracket-ending);
+                                string serviceAndEndpoint = endpoint.substr(ending, endBracket-ending);
                                 // Look for a '/'
-                                string::size_type slash = serviceAndPort.find("/");
+                                string::size_type slash = serviceAndEndpoint.find("/");
                                 if (slash != string::npos)
                                 {
-                                    serviceName = serviceAndPort.substr(0, slash);
+                                    serviceName = serviceAndEndpoint.substr(0, slash);
 
-                                    if ( (slash+1) < serviceAndPort.length())
+                                    if ( (slash+1) < serviceAndEndpoint.length())
                                     {
-                                        portName = serviceAndPort.substr(slash+1);
+                                        endpointName = serviceAndEndpoint.substr(slash+1);
                                     }
                                     else
                                     {
-                                        portName = "";
+                                        endpointName = "";
                                     }
 
                                 }
                                 else
                                 {
                                     // No '/' so all of it is the service name
-                                    serviceName = serviceAndPort;
-                                    portName = "";
+                                    serviceName = serviceAndEndpoint;
+                                    endpointName = "";
 
                                 }
                             }
@@ -90,14 +90,14 @@ namespace tuscany
                             {
                                 // Nothing between the ()
                                 serviceName = "";
-                                portName = "";
+                                endpointName = "";
                             }
                         }
                         else
                         {
                             // not the correct characters after the #, ignore the rest
                             serviceName = "";
-                            portName = "";
+                            endpointName = "";
                         }
                         
                     }
@@ -105,15 +105,15 @@ namespace tuscany
                     {
                         // Nothing after the hash
                         serviceName = "";
-                        portName = "";
+                        endpointName = "";
                     }
                 }
                 else
                 {
                     // No hash at all
-                    wsdlNamespaceURL = port;
+                    wsdlNamespaceURL = endpoint;
                     serviceName = "";
-                    portName = "";
+                    endpointName = "";
                 }
             }
 

sca/runtime/extensions/ws/reference/axis2c/src/tuscany/sca/ws/model/WSServiceBinding.h
@@ -42,11 +42,11 @@ namespace tuscany
                 /**
                  * Constructor.
                  * @param uri The uri of the binding.
-                 * @param port The definition of the port to which the entrypoint
-                 * or external service is to be bound. This is of the form
-                 * "namespace"#endpoint("service"/"port")
+                 * @param endpoint The definition of the endpoint to which the service
+                 * or reference is to be bound. This is of the form
+                 * "namespace"#endpoint("service"/"endpoint")
                  */
-                WSServiceBinding(model::Service* service, const string& uri, const string& port);  
+                WSServiceBinding(model::Service* service, const string& uri, const string& endpoint, const string& version);  
 
                 /**
                  * Destructor.
@@ -65,50 +65,61 @@ namespace tuscany
                  virtual ServiceWrapper* getServiceWrapper();
                                 
                 /**
-                 * Return the part of the port definition describing the wsdl 
+                 * Return the part of the endpoint definition describing the wsdl 
                  * namespace.
                  * @return The wsdl namespace.
                  */
                 string getWSDLNamespaceURL() const { return wsdlNamespaceURL; };
 
                 /**
-                 * Return the service part of the port definition.
+                 * Return the service part of the endpoint definition.
                  * @return The service to use.
                  */
                 string getServiceName() const { return serviceName; };
 
                 /**
-                 * Return the port name part of the port definition.
-                 * @return The port name to use.
+                 * Return the endpoint name part of the endpoint definition.
+                 * @return The endpoint name to use.
                  */
-                string getPortName() const { return portName; };
+                string getEndpointName() const { return endpointName; };
+                
+                /**
+                 * Return the SOAP version.
+                 * @return The SOAP version to use.
+                 */
+                string getSOAPVersion() const { return soapVersion; };
                 
             private:
             
                 /**
-                 * Parse the port specification.
+                 * Parse the endpoint specification.
                  */
-                void parsePort();
+                void parseEndpoint();
             
                 /**
-                 * The full port string.
+                 * The full endpoint string.
                  */
-                string port;
+                string endpoint;
 
                 /**
-                 * Namespace from the port.
+                 * Namespace from the endpoint.
                  */
                 string wsdlNamespaceURL;
 
                 /**
-                 * Service name from the port.
+                 * Service name from the endpoint.
                  */
                 string serviceName;
 
                 /**
-                 * Port name from the port.
+                 * WSDL Endpoint name.
                  */
-                string portName;
+                string endpointName;
+
+                /**
+                 * SOAP version.
+                 */
+                string soapVersion;
 
                 /**
                  * The wrapper for the service configured by this binding.

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/WSReferenceBindingExtension.cpp
@@ -71,9 +71,19 @@ namespace tuscany
                 if (bindingType == "WebServiceBinding")
                 {
                     string uri = scdlBinding->getCString("uri");
-                    string port = scdlBinding->getCString("port");
+                    string endpoint = scdlBinding->getCString("endpoint");
+                   commonj::sdo::DataObjectList& soap = scdlBinding->getList("soapbinding");
+                    string version;
+                    if (soap.size()!=0)
+                    {
+                        version = soap.getCString(0);
+                    }
+                    else
+                    {
+                        version = "";
+                    }
 
-                    WSReferenceBinding* serviceBinding = new WSReferenceBinding(reference, uri, port);
+                    WSReferenceBinding* serviceBinding = new WSReferenceBinding(reference, uri, endpoint, version);
                     
                     return serviceBinding;
                 }

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/model/WSReferenceBinding.cpp
@@ -30,57 +30,57 @@ namespace tuscany
         {
 
             // Constructor
-            WSReferenceBinding::WSReferenceBinding(Reference* reference, const string& uri, const string& port)
-                : ReferenceBinding(reference, uri), port(port)
+            WSReferenceBinding::WSReferenceBinding(Reference* reference, const string& uri, const string& endpoint, const string&version)
+                : ReferenceBinding(reference, uri), endpoint(endpoint), soapVersion(version)
             {
-                parsePort();
+                parseEndpoint();
             }
             
-            void WSReferenceBinding::parsePort()
+            void WSReferenceBinding::parseEndpoint()
             {
-                // Port is of the form: <wsdl-namepace-uri>#wsdl.endpoint(<service-name>/<port-name>)
-                string::size_type hash = port.find("#");
+                // Endpoint is of the form: <wsdl-namepace-uri>#wsdl.endpoint(<service-name>/<endpoint-name>)
+                string::size_type hash = endpoint.find("#");
                 if (hash != string::npos)
                 {
                     // Found a hash
 
                     // Namepace is the part before the #
-                    wsdlNamespaceURL = port.substr(0, hash);
+                    wsdlNamespaceURL = endpoint.substr(0, hash);
 
                     
-                    if ( (hash+1) < port.length())
+                    if ( (hash+1) < endpoint.length())
                     {
                         // Check the next part is wsdl.endpoint( 
                         int ending = hash+15;
-                        string check = port.substr(hash+1, 14);
+                        string check = endpoint.substr(hash+1, 14);
                         if (check.compare("wsdl.endpoint(") == 0)
                         {
                             // Find the matching )
-                            int endBracket = port.find(")",ending);
+                            int endBracket = endpoint.find(")",ending);
                             if (endBracket-1 > ending+1)
                             {
-                                string serviceAndPort = port.substr(ending, endBracket-ending);
+                                string serviceAndEndpoint = endpoint.substr(ending, endBracket-ending);
                                 // Look for a '/'
-                                string::size_type slash = serviceAndPort.find("/");
+                                string::size_type slash = serviceAndEndpoint.find("/");
                                 if (slash != string::npos)
                                 {
-                                    serviceName = serviceAndPort.substr(0, slash);
+                                    serviceName = serviceAndEndpoint.substr(0, slash);
 
-                                    if ( (slash+1) < serviceAndPort.length())
+                                    if ( (slash+1) < serviceAndEndpoint.length())
                                     {
-                                        portName = serviceAndPort.substr(slash+1);
+                                        endpointName = serviceAndEndpoint.substr(slash+1);
                                     }
                                     else
                                     {
-                                        portName = "";
+                                        endpointName = "";
                                     }
 
                                 }
                                 else
                                 {
                                     // No '/' so all of it is the service name
-                                    serviceName = serviceAndPort;
-                                    portName = "";
+                                    serviceName = serviceAndEndpoint;
+                                    endpointName = "";
 
                                 }
                             }
@@ -88,14 +88,14 @@ namespace tuscany
                             {
                                 // Nothing between the ()
                                 serviceName = "";
-                                portName = "";
+                                endpointName = "";
                             }
                         }
                         else
                         {
                             // not the correct characters after the #, ignore the rest
                             serviceName = "";
-                            portName = "";
+                            endpointName = "";
                         }
                         
                     }
@@ -103,15 +103,15 @@ namespace tuscany
                     {
                         // Nothing after the hash
                         serviceName = "";
-                        portName = "";
+                        endpointName = "";
                     }
                 }
                 else
                 {
                     // No hash at all
-                    wsdlNamespaceURL = port;
+                    wsdlNamespaceURL = endpoint;
                     serviceName = "";
-                    portName = "";
+                    endpointName = "";
                 }
             }
 

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/model/WSReferenceBinding.h
@@ -43,11 +43,11 @@ namespace tuscany
                 /**
                  * Constructor.
                  * @param uri The uri of the binding.
-                 * @param port The definition of the port to which the entrypoint
+                 * @param endpoint The definition of the endpoint to which the entrypoint
                  * or external service is to be bound. This is of the form
-                 * "namespace"#endpoint("service"/"port")
+                 * "namespace"#endpoint("service"/"endpoint")
                  */
-                WSReferenceBinding(Reference* reference, const string&uri, const string& port);  
+                WSReferenceBinding(Reference* reference, const string&uri, const string& endpoint, const string& version);  
 
                 /**
                  * Destructor.
@@ -71,23 +71,29 @@ namespace tuscany
                  virtual ServiceProxy* getServiceProxy();
                                 
                 /**
-                 * Return the part of the port definition describing the wsdl 
+                 * Return the part of the endpoint definition describing the wsdl 
                  * namespace.
                  * @return The wsdl namespace.
                  */
                 string getWSDLNamespaceURL() const { return wsdlNamespaceURL; };
 
                 /**
-                 * Return the service part of the port definition.
+                 * Return the service part of the endpoint definition.
                  * @return The service to use.
                  */
                 string getServiceName() const { return serviceName; };
 
                 /**
-                 * Return the port name part of the port definition.
-                 * @return The port name to use.
+                 * Return the endpoint name part of the endpoint definition.
+                 * @return The endpoint name to use.
                  */
-                string getPortName() const { return portName; };
+                string getEndpointName() const { return endpointName; };
+                
+                /**
+                 * Return the SOAP version.
+                 * @return The SOAP version to use.
+                 */
+                string getSOAPVersion() const { return soapVersion; };
                 
                  /**
                   * Returns the target service binding.
@@ -97,29 +103,34 @@ namespace tuscany
             private:
             
                 /**
-                 * Parse the port specification.
+                 * Parse the endpoint specification.
                  */
-                void parsePort();
+                void parseEndpoint();
             
                 /**
-                 * The full port string.
+                 * The full endpoint string.
                  */
-                string port;
+                string endpoint;
 
                 /**
-                 * Namespace from the port.
+                 * Namespace from the endpoint.
                  */
                 string wsdlNamespaceURL;
 
                 /**
-                 * Service name from the port.
+                 * Service name from the endpoint.
                  */
                 string serviceName;
 
                 /**
-                 * Port name from the port.
+                 * Endpoint name from the endpoint.
                  */
-                string portName;
+                string endpointName;
+
+                /**
+                 * SOAP version.
+                 */
+                string soapVersion;
 
                 /**
                  * The proxy representing the reference to the client

commit 34682945c2e9112f2300dc464a2a298fd0ffab09

Fixed build bug discovered by Windows build introduced in r448463 for new-style module based Axis2c services
git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@448515 13f79535-47bb-0310-9956-ffa450edef68

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/Axis2Dispatcher.cpp
@@ -49,7 +49,7 @@ Axis2Dispatcher_find_op(
     const axis2_env_t *env,
     axis2_svc_t *svc);
 
-AXIS2_EXTERN axis2_handler_t* AXIS2_CALL
+AXIS2_EXPORT axis2_handler_t* AXIS2_CALL
 Axis2Dispatcher_create(const axis2_env_t *env, 
                          axis2_qname_t *qname) 
 {

sca/runtime/extensions/ws/service/axis2c/src/tuscany/sca/ws/Axis2Service.cpp
@@ -245,7 +245,7 @@ Axis2Service_invoke(axis2_svc_skeleton_t *svc_skeleton,
                         axis2_endpoint_ref_t *endpoint_ref = NULL;
                         endpoint_ref = AXIS2_MSG_CTX_GET_FROM(msg_ctx, env);
                         axis2_char_t *address = NULL;        
-                        address = AXIS2_ENDPOINT_REF_GET_ADDRESS(endpoint_ref, env);
+                        address = (axis2_char_t*) AXIS2_ENDPOINT_REF_GET_ADDRESS(endpoint_ref, env);
                         axis2_char_t **url_tokens = NULL;
                         url_tokens = axis2_parse_request_url_for_svc_and_op(env, address);
                         string service(url_tokens[0]);