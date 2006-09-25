4e10e2ace5d2d25e63e414bbd3b0723ba7d5fb91
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-09-25 07:13:09 +0000

    Initial implementation of default binding. Added options to configure.ac to allow for optional building of bindings and component types
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@449587 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sca/runtime/extensions/sca/service/axis2c/src/tuscany/sca/binding/SCAReferenceBindingExtension.cpp
@@ -0,0 +1,94 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+*   
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+#include "SCAReferenceBindingExtension.h"
+#include "model/SCAReferenceBinding.h"
+#include "tuscany/sca/util/Logging.h"
+#include "tuscany/sca/util/Utils.h"
+#include "tuscany/sca/core/SCARuntime.h"
+
+
+extern "C"
+{
+    #if defined(WIN32) || defined(_WINDOWS)
+    __declspec(dllexport) 
+    #endif
+        void tuscany_sca_extension_initialize()
+    {
+        tuscany::sca::binding::SCAReferenceBindingExtension::initialize();
+    }
+}
+
+namespace tuscany
+{
+    namespace sca
+    {
+        namespace binding
+        {
+            // ===================================================================
+            // Constructor for the SCAReferenceBinding class. 
+            // ===================================================================
+            SCAReferenceBindingExtension::SCAReferenceBindingExtension()
+            { 
+                LOGENTRY(1, "SCAReferenceBindingExtension::constructor");     
+                LOGEXIT(1, "SCAReferenceBindingExtension::constructor");
+            }
+            
+            // ===================================================================
+            // Destructor for the SCAReferenceBindingExtension class.
+            // ===================================================================
+            SCAReferenceBindingExtension::~SCAReferenceBindingExtension()
+            { 
+                LOGENTRY(1, "SCAReferenceBindingExtension::destructor");;           
+                LOGEXIT(1, "SCAReferenceBindingExtension::destructor");
+            }
+
+            const string SCAReferenceBindingExtension::extensionName("sca");
+            const string SCAReferenceBindingExtension::typeQName("http://www.osoa.org/xmlns/sca/1.0#SCABinding");
+
+            // ===================================================================
+            // loadModelElement - load the info from binding.ws
+            // ===================================================================
+            ReferenceBinding* SCAReferenceBindingExtension::getReferenceBinding(Composite *composite, Reference* reference, DataObjectPtr scdlBinding)
+            {
+                string bindingType = scdlBinding->getType().getName();
+                if (bindingType == "SCABinding")
+                {
+                    string uri = scdlBinding->getCString("uri");
+
+                    SCAReferenceBinding* serviceBinding = new SCAReferenceBinding(reference, uri);
+                    
+                    return serviceBinding;
+                }
+                else
+                {
+                    return NULL;
+                }
+            }
+
+           void SCAReferenceBindingExtension::initialize()
+            { 
+                LOGENTRY(1, "SCAReferenceBindingExtension::initialize");;           
+                SCARuntime::getInstance()->registerReferenceBindingExtension(new SCAReferenceBindingExtension());
+                LOGEXIT(1, "SCAReferenceBindingExtension::initialize");;           
+            }
+
+        } // End namespace binding
+    } // End namespace sca
+} // End namespace tuscany
