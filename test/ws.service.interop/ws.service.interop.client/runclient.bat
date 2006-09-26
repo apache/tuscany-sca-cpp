ac4aa94f93115db43267b07a5cfad50e8ee355d4
Andrew Borley <ajborley@apache.org>
2006-09-26 15:28:00 +0000

    New interop test. Reads in XML files as SDO, converts to Axiom, sends to service. At service Axiom gets converted to SDO, SDO is compared with data from XML file (read in as SDO itself) & SDO is returned as Axiom (unless comparison failure occurs). At client returned Axiom gets converted to SDO and is compared with the original SDO.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@450083 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/ws.service.interop/ws.service.interop.client/runclient.bat
@@ -0,0 +1,27 @@
+@echo off
+
+@REM  Licensed under the Apache License, Version 2.0 (the "License");
+@REM  you may not use this file except in compliance with the License.
+@REM  You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM  Unless required by applicable law or agreed to in writing, software
+@REM  distributed under the License is distributed on an "AS IS" BASIS,
+@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM  See the License for the specific language governing permissions and
+@REM  limitations under the License.
+
+setlocal
+
+if "%AXIS2C_HOME%" == "" (
+echo "AXIS2C_HOME not set"
+goto end
+)
+echo Using Axis2C installed at %AXIS2C_HOME%
+
+rem Run the client
+.\ws.service.interop.client.exe
+
+:end
+endlocal
---
+++ test/ws.service.interop/ws.service.interop/Test.h
@@ -0,0 +1,82 @@
+/* 
++----------------------------------------------------------------------+ 
+|                                                                      | 
+| Licensed under the Apache License, Version 2.0 (the "License"); you  | 
+| may not use this file except in compliance with the License. You may | 
+| obtain a copy of the License at                                      | 
+|  http://www.apache.org/licenses/LICENSE-2.0                          |
+|                                                                      | 
+| Unless required by applicable law or agreed to in writing, software  | 
+| distributed under the License is distributed on an "AS IS" BASIS,    | 
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      | 
+| implied. See the License for the specific language governing         | 
+| permissions and limitations under the License.                       | 
++----------------------------------------------------------------------+ 
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <commonj/sdo/SDO.h>
+#include <stdio.h>
+#include <string>
+using std::string;
+
+class Test  
+{
+public:
+    virtual DataObjectPtr Interop01(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop02(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop03(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop04(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop05(DataObjectPtr arg1) = 0;
+    virtual string Interop06(string arg1) = 0;
+    virtual DataObjectPtr Interop07(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop08(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop09(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop10(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop11(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop12(DataObjectPtr arg1) = 0;
+    //virtual DataObjectPtr Interop13(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop14(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop15(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop16(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop17(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop18(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop19(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop20(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop21(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop22(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop23(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop24(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop25(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop26(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop27(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop28(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop29(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop30(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop31(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop32(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop33(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop34(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop35(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop36(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop37(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop38(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop39(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop40(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop41(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop42(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop43(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop44(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop45(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop46(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop47(DataObjectPtr arg1) = 0;
+    //virtual DataObjectPtr Interop48(DataObjectPtr arg1) = 0;
+    //virtual DataObjectPtr Interop49(DataObjectPtr arg1) = 0;
+    virtual DataObjectPtr Interop50(DataObjectPtr arg1) = 0;
+};
+
+#endif
+
+
