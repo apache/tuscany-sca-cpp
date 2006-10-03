8bf85f1444efcfd44bdb01f2435f297dd04e5e00
Peter George Robbins <robbinspg@apache.org>
2006-10-03 08:17:36 +0000

    Update svn-properties
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@452342 13f79535-47bb-0310-9956-ffa450edef68

--- sca/runtime/core/src/tuscany/sca/core/ServiceWrapper.cpp
+++ sca/runtime/core/src/tuscany/sca/core/ServiceWrapper.cpp
@@ -15,7 +15,7 @@
  *  limitations under the License.
  */
 
-/* $Rev$ $Date: 2005/12/22 11:33:21 $ */
+/* $Rev$ $Date$ */
 
 #include "tuscany/sca/core/ServiceWrapper.h"
 #include "tuscany/sca/util/Logging.h"
