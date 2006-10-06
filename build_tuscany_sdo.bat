0c7899932b5a676eaffc844de3a7080804da1121
Peter George Robbins <robbinspg@apache.org>
2006-10-06 17:19:05 +0000

    TUSCANY-793 package build for sca
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@453687 13f79535-47bb-0310-9956-ffa450edef68

--- build_tuscany_sdo.bat
+++ build_tuscany_sdo.bat
@@ -123,7 +123,7 @@ if not exist %TO_BIN%\%BIN_PACKAGE%\samples\ides mkdir %TO_BIN%\%BIN_PACKAGE%\sa
 if not exist %TO_BIN%\%BIN_PACKAGE%\samples\misc mkdir %TO_BIN%\%BIN_PACKAGE%\samples\misc
 
 xcopy/s %FROM_DIR%\samples\ides %TO_BIN%\%BIN_PACKAGE%\samples\ides
-xcopy/s %FROM_DIR%\samples\misc %TO_BIN%\%BIN_PACKAGE%\samples\misc
+xcopy/s /Y %FROM_DIR%\samples\misc %TO_BIN%\%BIN_PACKAGE%\samples\misc
 copy %FROM_DIR%\samples\INSTALL   %TO_BIN%\%BIN_PACKAGE%\samples
 copy %FROM_DIR%\samples\LICENSE   %TO_BIN%\%BIN_PACKAGE%\samples
 copy %FROM_DIR%\samples\COPYING   %TO_BIN%\%BIN_PACKAGE%\samples
