4301426a32a394a7d146bc4d089041944753fba3
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-10-06 06:21:29 +0000

    Adjusted the Windows deploy scripts to the new structure of the samples.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@453508 13f79535-47bb-0310-9956-ffa450edef68

--- sca/samples/PythonCalculator/deploy.cmd
+++ sca/samples/PythonCalculator/deploy.cmd
@@ -34,20 +34,18 @@ copy %sourcePath%\sample.calculator\*.componentType %destinationPath%\sample.cal
 copy %sourcePath%\sample.calculator\*.composite     %destinationPath%\sample.calculator
 copy %sourcePath%\sample.calculator\*.py            %destinationPath%\sample.calculator
 copy %sourcePath%\sample.calculator\Calculator.wsdl %destinationPath%\sample.calculator
+copy %sourcePath%\sample.calculator\runwsserver.bat          %destinationPath%\sample.calculator
 
 if not exist %destinationPath%\sample.calculator.client mkdir %destinationPath%\sample.calculator.client
 copy %sourcePath%\sample.calculator.client\calculator_client.py %destinationPath%\sample.calculator.client
+copy %sourcePath%\sample.calculator.client\runclient.bat     %destinationPath%\sample.calculator.client
 
 if not exist %destinationPath%\sample.calculator.wsclient mkdir %destinationPath%\sample.calculator.wsclient
 copy %sourcePath%\sample.calculator.wsclient\calculator_wsclient.py               %destinationPath%\sample.calculator.wsclient
 copy %sourcePath%\sample.calculator.wsclient\sample.calculator.wsclient.composite %destinationPath%\sample.calculator.wsclient
 copy %sourcePath%\sample.calculator.wsclient\Calculator.wsdl                      %destinationPath%\sample.calculator.wsclient
+copy %sourcePath%\sample.calculator.wsclient\runwsclient.bat %destinationPath%\sample.calculator.wsclient
 
 copy %sourcePath%\*.composite %destinationPath%
-
-if not exist %destinationPath%\bin mkdir %destinationPath%\bin
-copy %sourcePath%\sample.calculator.client\runclient.bat     %destinationPath%\bin
-copy %sourcePath%\sample.calculator\runwsserver.bat          %destinationPath%\bin
-copy %sourcePath%\sample.calculator.wsclient\runwsclient.bat %destinationPath%\bin
 
 endlocal
