b0e539ac21280e65a00418c4e5cd3aac3d00cc07
Andrew Borley <ajborley@apache.org>
2006-10-10 16:08:43 +0000

    Fixed incorrect paths in runwsclient windows batch file
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp@454803 13f79535-47bb-0310-9956-ffa450edef68

--- sca/samples/RubyCalculator/sample.calculator.wsclient/runwsclient.bat
+++ sca/samples/RubyCalculator/sample.calculator.wsclient/runwsclient.bat
@@ -38,12 +38,12 @@ goto end
 echo using Axis2C installed at %AXIS2C_HOME%
 
 set TUSCANY_SCACPP_SYSTEM_ROOT=%~d0%~p0\..\
-set TUSCANY_SCACPP_DEFAULT_COMPONENT=sample.calculator.CalculatorComponent
+set TUSCANY_SCACPP_DEFAULT_COMPONENT=sample.calculator.CalculatorWSClientComponent
 
 set PATH=%TUSCANY_SCACPP%\bin;%TUSCANY_SDOCPP%\bin;%AXIS2C_HOME%\lib;%PATH%
 
 cd %TUSCANY_SCACPP_SYSTEM_ROOT%\sample.calculator.wsclient
-ruby -I%TUSCANY_SCACPP%\extensions\ruby\lib CalculatorWSClient.rb
+ruby -I%TUSCANY_SCACPP%\extensions\ruby\bin CalculatorWSClient.rb
 
 :end
 endlocal
