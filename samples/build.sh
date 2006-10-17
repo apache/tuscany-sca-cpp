93581772a5221704a1c0baa320b3223aeca0a40f
Andrew Borley <ajborley@apache.org>
2006-10-17 12:00:58 +0000

    Make sure the Python and Ruby extensions/samples are built in a distribution. Also enable sample building to check for and build Ruby & Python samples if the approproate env variables are set
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@464910 13f79535-47bb-0310-9956-ffa450edef68

--- samples/build.sh
+++ samples/build.sh
@@ -36,7 +36,27 @@ exit;
 fi
 echo "Using Axis2C installed at $AXIS2C_HOME"
 
-./configure --prefix=${TUSCANY_SCACPP} --enable-static=no
+if [ x$PYTHON_LIB = x ]; then
+echo "PYTHON_LIB not set. Python extension will not be built"
+elif [ x$PYTHON_INCLUDE = x ]; then
+echo "PYTHON_INCLUDE not set. Python extension will not be built"
+elif [ x$PYTHON_VERSION = x ]; then
+echo "PYTHON_VERSION not set. Python extension will not be built"
+else
+echo "Building PYTHON extension with Python $PYTHON_VERSION installed at $PYTHON_LIB, $PYTHON_INCLUDE"
+ENABLE_PYTHON=--enable-python
+fi
+
+if [ x$RUBY_LIB = x ]; then
+echo "RUBY_LIB not set. Ruby extension will not be built"
+elif [ x$RUBY_INCLUDE = x ]; then
+echo "RUBY_INCLUDE not set. Ruby extension will not be built"
+else
+echo "Building Ruby extension with Ruby installed at $RUBY_LIB, $RUBY_INCLUDE"
+ENABLE_RUBY=--enable-ruby
+fi
+
+./configure --prefix=${TUSCANY_SCACPP} --enable-static=no ${ENABLE_RUBY} ${ENABLE_PYTHON}
 make
 make install
 
