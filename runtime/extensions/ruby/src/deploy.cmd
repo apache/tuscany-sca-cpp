86fe2b6db6f044e59a214bb4a432deb94b189579
Andrew Borley <ajborley@apache.org>
2006-10-18 18:43:12 +0000

    Added missing files & corrected build files for the fix to TUSCANY-868
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@465329 13f79535-47bb-0310-9956-ffa450edef68

---
+++ runtime/extensions/ruby/src/deploy.cmd
@@ -0,0 +1,28 @@
+@echo off
+@REM  Licensed to the Apache Software Foundation (ASF) under one
+@REM  or more contributor license agreements.  See the NOTICE file
+@REM  distributed with this work for additional information
+@REM  regarding copyright ownership.  The ASF licenses this file
+@REM  to you under the Apache License, Version 2.0 (the
+@REM  "License"); you may not use this file except in compliance
+@REM  with the License.  You may obtain a copy of the License at
+@REM  
+@REM    http://www.apache.org/licenses/LICENSE-2.0
+@REM    
+@REM  Unless required by applicable law or agreed to in writing,
+@REM  software distributed under the License is distributed on an
+@REM  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM  KIND, either express or implied.  See the License for the
+@REM  specific language governing permissions and limitations
+@REM  under the License.
+
+
+setlocal
+set currentPath=%~d0%~p0
+
+echo Enabling the Tuscany Ruby extension
+rename %currentPath%\bin\tuscany_sca_ruby.dll.disabled tuscany_sca_ruby.dll
+
+:end
+echo Done
+endlocal
