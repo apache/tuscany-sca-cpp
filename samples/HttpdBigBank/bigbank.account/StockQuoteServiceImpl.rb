e5bd35ed5d6ad231c068d602b67a9e4513f96943
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-11-19 22:33:09 +0000

    A more distributed version of Bigbank, with the account data service and account service in different composites, configured to run behind the Apache HTTPD server.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@476957 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/HttpdBigBank/bigbank.account/StockQuoteServiceImpl.rb
@@ -0,0 +1,52 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+require "rexml/document"
+include REXML
+
+class StockQuoteServiceImpl
+
+  attr_writer :webService
+
+  def initialize()
+    print "Ruby - StockQuoteServiceImpl.initialize\n"
+  end
+
+  def getQuote(symbol)
+    print "Ruby - StockQuoteServiceImpl.getQuote ", symbol, "\n"
+
+    price = 0
+    catch (:RuntimeError) do
+      begin
+      
+        result = @webService.GetQuote(symbol)
+        doc = Document.new(result)
+        price = doc.root.elements["Stock"].elements["Last"].text;
+        
+      rescue
+        # offline or StockQuote web service is down
+        price = 80.0
+      end
+    end  
+    
+    print "Ruby - stock price = ", price, "\n"
+    
+    return price.to_f
+  end
+	
+end
