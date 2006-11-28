6fb298b798f463fe44809e355303541e624d348c
Andrew Borley <ajborley@apache.org>
2006-11-28 11:18:45 +0000

    Added WeatherForecast Python sample
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@479990 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/WeatherForecast/sample.weather/WeatherForecastImpl.py
@@ -0,0 +1,65 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#
+# 
+# 
+# This Python code is a simple sample that calls a service that returns an 
+# SDO which has been converted into an ElementTree Element object. The Element
+# object is then converted into formatted text
+ 
+import xml.etree.ElementTree
+
+def getForecastByPlaceName(placeName):
+
+    print "Python - WeatherForecastImpl.getForecastByPlaceName:", placeName
+
+    xmlResult = weatherWebService.GetWeatherByPlaceName(placeName)    
+
+    print "Python - Formatting result from weatherService:", xmlResult
+
+    return formatWeather(xmlResult, placeName)
+
+
+def getForecastByZipCode(zipCode):
+
+    print "Python - WeatherForecastImpl.getForecastByZipCode:", zipCode
+
+    xmlResult = weatherWebService.GetWeatherByZipCode(zipCode)    
+
+    print "Python - Formatting result from weatherService:", xmlResult
+
+    return formatWeather(xmlResult, zipCode)
+
+
+def formatWeather(weather, name):
+
+    result = "Could not retrieve Weather Forecast for " + str(name)
+
+    pathAndNS = "./{http://www.webservicex.net}"
+
+    if weather.findtext(pathAndNS+"PlaceName"):
+    
+        result = "\n\nWeather Forecast for "+ weather.findtext(pathAndNS+"PlaceName")+ ", "+ weather.findtext(pathAndNS+"StateCode")+"\n"
+        result += "Latitude: "+ weather.findtext(pathAndNS+"Latitude")+ " Longitude: "+ weather.findtext(pathAndNS+"Longitude")+"\n\n"
+    
+        for node in weather.findall(".//{http://www.webservicex.net}WeatherData"):
+            result += node.findtext(pathAndNS+"Day")
+            result += ":\tMax "+ node.findtext(pathAndNS+"MaxTemperatureC")
+            result += "C. Min "+ node.findtext(pathAndNS+"MinTemperatureC")+ "C.\n"
+
+    return result
