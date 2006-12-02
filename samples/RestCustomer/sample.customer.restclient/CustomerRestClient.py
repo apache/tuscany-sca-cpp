9c624ea8dc77d9779b36d9ae0ba7ff4aa36c77af
Jean Sebastien Delfino <jsdelfino@apache.org>
2006-12-02 21:43:28 +0000

    Cleaned up the REST sample and added a command style service to show the XML/HTTP command style support.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@481633 13f79535-47bb-0310-9956-ffa450edef68

--- samples/RestCustomer/sample.customer.restclient/CustomerRestClient.py
+++ samples/RestCustomer/sample.customer.restclient/CustomerRestClient.py
@@ -25,30 +25,62 @@
 from xml.etree import ElementTree as et
 import sca
 
-# Locate the customer service
-customerService = sca.locateservice("Customer")
+# Locate the customer resource service
+customerResource = sca.locateservice("CustomerResource")
 
-# Invoke the CRUD operations on the customer resource
+# Show how to invoke CRUD operations on the customer resource
+# The CRUD operations translate to HTTP POST, GET, PUT and DELETE
+# according to the REST pattern
 
-customer = customerService.retrieve("2345")
-print "Retrieved customer " + et.tostring(customer)
+customer = customerResource.retrieve("2345")
+print "Rest - Retrieved customer " + et.tostring(customer)
 
 customer = et.fromstring("""<customer xmlns="http://sample.customer"><id>1234</id><firstName>Jane</firstName><lastName>Doe</lastName></customer>""")
-url = customerService.create(customer)
-print "Created customer " + url
+url = customerResource.create(customer)
+print "Rest - Created customer " + url
 
-customer = customerService.retrieve("1234")
-print "Retrieved customer " + et.tostring(customer)
+customer = customerResource.retrieve("1234")
+print "Rest - Retrieved customer " + et.tostring(customer)
 
-customer = customerService.retrieve(url)
-print "Retrieved by url " + et.tostring(customer)
+customer = customerResource.retrieve(url)
+print "Rest - Retrieved by url " + et.tostring(customer)
 
 customer.find("{http://sample.customer}lastName").text="Smith"
-customerService.update("1234", customer)
-print "Updated customer 1234"
+customerResource.update("1234", customer)
+print "Rest - Updated customer 1234"
 
-customer = customerService.retrieve("1234")
-print "Retrieved customer " + et.tostring(customer)
+customer = customerResource.retrieve("1234")
+print "Rest - Retrieved customer " + et.tostring(customer)
 
-customerService.delete("1234")
-print "Deleted customer 1234"
+customerResource.delete("1234")
+print "Rest - Deleted customer 1234"
+
+# Also show how to use REST binding to invoke remote commands
+# using HTTP GET and XML over HTTP POST, the REST binding
+# uses that command pattern when you don't declare a REST interface
+# on your SCA reference
+
+# Locate the customer command service
+customerCommand = sca.locateservice("CustomerCommand")
+
+# Invoke operations on the customer command service
+customer = customerCommand.retrieve("2345")
+print "Command  - Retrieved customer " + et.tostring(customer)
+
+customer = et.fromstring("""<customer xmlns="http://sample.customer"><id>1234</id><firstName>Jane</firstName><lastName>Doe</lastName></customer>""")
+url = customerCommand.create(customer)
+print "Command  - Created customer " + url
+
+customer = customerCommand.retrieve("1234")
+print "Command  - Retrieved customer " + et.tostring(customer)
+
+customer.find("{http://sample.customer}lastName").text="Smith"
+# commented out for now as application/form-data does not work yet
+#customerCommand.update("1234", customer)
+#print "Command  - Updated customer 1234"
+
+customer = customerCommand.retrieve("1234")
+print "Command  - Retrieved customer " + et.tostring(customer)
+
+customerCommand.delete("1234")
+print "Command  - Deleted customer 1234"
