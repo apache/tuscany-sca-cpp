44b4c2bf87876c53825fc1c0b38054bd56211520
Simon Laws <slaws@apache.org>
2007-02-07 11:34:11 +0000

    Updated the PHPCalculator sample to build on windows.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/cpp/sca@504509 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/PHPCalculator/sample.calculator.client/runclient_cpp.bat
@@ -0,0 +1,7 @@
+set TUSCANY_SCACPP_ROOT=%TUSCANY_SCACPP%\samples\PHPCalculator\deploy
+set TUSCANY_SCACPP_COMPONENT=sample.calculator.CalculatorComponent
+set TUSCANY_SCACPP_LOGGING=9
+set PHRC=C:\simon\Projects\Tuscany\php\php-5.2.0\Debug_TS
+
+calculator_client.exe
+
