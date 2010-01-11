12f3b96b29adf92cc9931422f06f07bd6231f57f
Jean Sebastien Delfino <jsdelfino@apache.org>
2010-01-11 08:29:45 +0000

    Created component structure and build files for Web service component using Axis2/C.
    git-svn-id: https://svn.apache.org/repos/asf/tuscany/sca-cpp/trunk@897790 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/webservice/client-test.cpp
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* $Rev$ $Date$ */
+
+/**
+ * Test Web service component.
+ */
+
+#include <assert.h>
+#include "stream.hpp"
+#include "string.hpp"
+#include "list.hpp"
+#include "value.hpp"
+#include "monad.hpp"
+#include "perf.hpp"
+#include "../../modules/http/curl.hpp"
+
+namespace tuscany {
+namespace webservice {
+
+const string url("http://localhost:8090/echo");
+
+bool testEval() {
+    return true;
+}
+
+}
+}
+
+int main() {
+    tuscany::cout << "Testing..." << tuscany::endl;
+
+    tuscany::webservice::testEval();
+
+    tuscany::cout << "OK" << tuscany::endl;
+
+    return 0;
+}
